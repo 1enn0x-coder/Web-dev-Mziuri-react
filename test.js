75cc7e2f53372b936b82dc9d93858e2c3ea1fa33
1enn0x-coder <selfimprovementisthekey2life@gmail.com>
2025-10-20 18:40:27 +0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,29 @@
+ const person = {
+    name : "Alice",
+    age : 25,
+    city : "los Angeles"
+ };
+
+
+const { name , age , city} = await fetch('')
+
+
+print();
+
+function print(message = "hello world")
+{
+    console.log(message);
+}
+
+
+const numbers = [1,2,3];
+
+const doubled = numbers.map((n) => n * 2);
+const filtered = numbers.filter((n) => n % 2 === 0);
+
+
+console.log(doubled);
+
+
+
+
